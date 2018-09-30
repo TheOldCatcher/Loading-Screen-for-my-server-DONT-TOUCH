b43734d8008a92e669efca6c496c88c1cf4474cf
Catcher 13 <40405475+TheOldCatcher@users.noreply.github.com>
2018-09-30 13:24:20 -0400

    Initial commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+ /* An XML database can easily be read, therefore, private information should be set here */
+	
+    $user = "catcher"; // username
+	$passw = "catcher"; // password
+	$apikey = "3BA18DD1E3889BAB019152B5E6CD29F3"; // Find your API Key at https://steamcommunity.com/dev/apikey
+
+?>
